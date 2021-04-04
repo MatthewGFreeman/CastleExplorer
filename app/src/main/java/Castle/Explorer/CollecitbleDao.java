79c190dafe5b2d8f68542eb1171411f04620d7c5
Matthew Freeman <matthew.freeman@revature.net>
2021-04-04 18:14:07 -0400

    Adds implementation of Dao in classes

---
+++ app/src/main/java/Castle/Explorer/CollecitbleDao.java
@@ -0,0 +1,31 @@
+package Castle.Explorer;
+
+import java.util.List;
+
+public class CollecitbleDao implements Dao<Collectible> {
+
+    @Override
+    public void insert(Collectible e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Collectible> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(Collectible e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(Collectible e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ app/src/main/java/Castle/Explorer/FurnishingsDao.java
@@ -0,0 +1,31 @@
+package Castle.Explorer;
+
+import java.util.List;
+
+public class FurnishingsDao implements Dao<Furniture> {
+
+    @Override
+    public void insert(Furniture e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Furniture> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(Furniture e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(Furniture e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
