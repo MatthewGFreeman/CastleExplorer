358d2d9d8ae815694e4bca413778c3254ee7f6b5
Matthew Freeman <matthew.freeman@revature.net>
2021-04-05 11:11:52 -0400

    Adds getAll method for CollectibleDao

--- app/src/main/java/Castle/Explorer/CollecitbleDao.java
+++ app/src/main/java/Castle/Explorer/CollecitbleDao.java
@@ -1,9 +1,14 @@
 package Castle.Explorer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollecitbleDao implements Dao<Collectible> {
-
+    Connection connection;
     @Override
     public void insert(Collectible e) {
         // TODO Auto-generated method stub
@@ -12,8 +17,19 @@ public class CollecitbleDao implements Dao<Collectible> {
 
     @Override
     public List<Collectible> getAll() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Collectible> list = new ArrayList<>();
+        try {
+            PreparedStatement pStatement = connection.prepareStatement("select * from collectible");
+            ResultSet rSet = pStatement.executeQuery();
+            while (rSet.next()) {
+                Collectible temp = new Collectible(rSet.getString("name"));
+                list.add(temp);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
@@ -26,6 +42,13 @@ public class CollecitbleDao implements Dao<Collectible> {
     public void delete(Collectible e) {
         // TODO Auto-generated method stub
         
+    }
+
+    /**
+     * @param connection
+     */
+    public CollecitbleDao(Connection connection) {
+        this.connection = connection;
     }
     
 }
